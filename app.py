076f383cad120e3296aa41a92866f3ec67f1958e
Antonio Anaya <anaya.4589@gmail.com>
2023-05-25 02:41:05 -0600

    2023, fixed flask url

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def serve_static_html():
     # Serve the static HTML file from the 'static' folder
-    return send_from_directory('static', 'the-internet-of-production-alliance-files/open-know-where-2.html')
+    return send_from_directory('the-internet-of-production-alliance-files', 'open-know-where-2.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8001)
