daab466fc363e2a39aea67b76476575e7824b149
Antonio Anaya <anaya.4589@gmail.com>
2024-01-24 02:20:25 -0600

    2024, okw, deleted files merge prep

--- dev_libs/js_handler.py
+++
@@ -1,8 +0,0 @@
-with open('dev/cluster_icon_tr.js') as file:
-    create_cluster_icon_tr = file.read()
-
-with open('dev/cluster_icon_sq.js') as file:
-    create_cluster_icon_sq = file.read()
-
-with open('dev/cluster_icon_cr.js') as file:
-    create_cluster_icon_cr = file.read()
